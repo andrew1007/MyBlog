7871ed47f759551916c7874164baa279bfc08f9a
andrew1007 <yueh.andrew@gmail.com>
2017-09-01 14:36:07 -0700

    finished iterators article

--- iterators.js
+++ iterators.js
@@ -1,21 +1,65 @@
-new String()[Symbol.iterator] ? true : false
-new Array()[Symbol.iterator] ? true : false
-new Set()[Symbol.iterator] ? true : false
-new Map()[Symbol.iterator] ? true : false
-new Object()[Symbol.iterator] ? true : false
+<a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol" target="_blank">Symbols</a>&nbsp;were quietly added to ES6. As a web developer, they have low use cases. But <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol/iterator" target="_blank">Symbol.iterator</a>,&nbsp;embedded into certain object types, give developers access to ES6s new iterator.<br />
+<br />
+<b style="font-size: x-large;">The <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of" target="_blank">for...of</a>&nbsp;loop</b><br />
+This new loop is similar to <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach" target="_blank">.forEach</a>&nbsp;for arrays, but the main difference is that <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of" target="_blank">for...of</a> loops can be used on anything with a <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol/iterator" target="_blank">Symbol.iterator</a> attribute.<br />
+<pre data-codetype="auto"><code>for (let char of "console.logged character by character") {
+  console.log(char);
+}
+</code></pre>
+<pre data-codetype="auto"><code>new String()[Symbol.iterator] ? true : false // true
+new Array()[Symbol.iterator] ? true : false // true
+new Set()[Symbol.iterator] ? true : false // true
+new Map()[Symbol.iterator] ? true : false // true
+</code></pre>
+This means string, <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map" target="_blank">maps</a>, <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set" target="_blank">sets</a>, and arrays are all iteratable using <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of" target="_blank">for...of</a> loops.<br />
+<br />
+<span style="font-size: large;"><b><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from" target="_blank">Array.from</a> for objects that have a <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol/iterator" target="_blank">Symbol.iterator</a></b></span><br />
+<a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from" target="_blank">Array.from</a> is essentially a "mapped" version of what a <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of" target="_blank">for...of loop</a> does.<br />
+<pre data-codetype="auto"><code>Array.from("string") //['s',&nbsp;'t',&nbsp;'r',&nbsp;'i',&nbsp;'n',&nbsp;'g'&nbsp;]
+Array.from(new Set([1, 1, 3, 4, 5])) //[1,&nbsp;3,&nbsp;4,&nbsp;5&nbsp;] from a set to an array
 
-Array.from("a string that will be split")
-const map  = new Map
-map.set("key", "value")
-map.set("another key", "mapped as array pairs")
-Array.from(map)
-
-
-https://twitter.com/mraleph/status/411549064787152896?lang=en
-let array = [1,2,1,1,1,11]
-
-Array.from(new Set(array))
+const sampleMap = new Map()
+sampleMap.set(1, '1s value').set(2, '2s value').set(3, '3s value')
+//Map&nbsp;{&nbsp;1&nbsp;=&gt;&nbsp;'1s&nbsp;value',&nbsp;2&nbsp;=&gt;&nbsp;'2s&nbsp;value',&nbsp;3&nbsp;=&gt;&nbsp;'3s&nbsp;value'&nbsp;}
+Array.from(sampleMap)
+//[&nbsp;[1,&nbsp;'1s&nbsp;value'],&nbsp;[2,&nbsp;'2s&nbsp;value'],&nbsp;[3,&nbsp;'3s&nbsp;value']&nbsp;] key value pairs
+</code></pre>
+<br />
+<span style="font-size: large;"><b>Objects don't have a <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol/iterator" target="_blank">Symbol.iterator</a></b></span>
+<br />
+<pre data-codetype="auto"><code>new Object()[Symbol.iterator] ? true : false // false!?
+</code></pre>
+I found this surprising, but it's easy to circumvent and still use <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of" target="_blank">for...of</a> loops using <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys" target="_blank">Object.keys</a>, <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values" target="_blank">Object.values</a>, and <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries" target="_blank">Object.entries</a>.
+<br />
+<pre data-codetype="auto"><code>const sampleObject = {a: 1, b: 2, c: 3}
+Object.values(sampleObject) //[1, 2, 3]
+Object.keys(sampleObject) //['a', 'b', 'c']
+Object.entries(sampleObject) // [ ['a', 1], ['b', 2], ['c', 3] ]
+</code></pre>
+<br />
+<span style="font-size: large;"><b><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment" target="_blank">Destructure assignment</a> for iterators</b></span><br />
+This is not exclusive to <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of" target="_blank">for...of</a> loops, but it's nice to point out that the ES6 <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment" target="_blank">destructure assignment</a> works on iterators for variable declaration.
+<br />
+<pre data-codetype="auto"><code>const sampleMap = new Map()
+sampleMap.set(1, '1s value').set(2, '2s value').set(3, '3s value')
+for (let [key, val] of sampleMap) {
+  console.log(key)
+  console.log(val)
+}
+</code></pre>
+<br />
+<span style="font-size: large;"><b>Performance</b></span><br />
+In its current state with transpilers, ES6 gets outperformed by raw ES5. In some cases <a target="_blank" href="http://incaseofstairs.com/2015/06/es6-feature-performance/" target="_blank">it's by a lot</a>. But in my opinion, it's usually worth it for cleanliness and maintainability. It also makes me think of this quote:<br />
+<blockquote>
+<a target="_blank" href="https://twitter.com/mraleph/status/411549064787152896?lang=en" target="_blank">Often devs still approach performance of JS code as if they are riding a horse cart but the horse had long been replaced with fusion reactor</a>
+</blockquote>
+Before I conclude, let me show you how to create an array of only unique values with the power of ES6:
+<br />
+<pre data-codetype="auto"><code>const sampleArray = [1 , 2, 1, 1, 1, 11]
+Array.from(new Set(sampleArray))
 //or even better, using spread
-[...new Set(array)]
-
-Array.from("a new string")
+[...new Set(sampleArray)]
+//[1,&nbsp;2,&nbsp;11]
+</code></pre>
+<br />
+Happy coding!

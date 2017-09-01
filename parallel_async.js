ca00eb07249dfdd409c8e0318cc33a0d2af2e08a
andrew1007 <yueh.andrew@gmail.com>
2017-08-31 18:59:48 -0700

    stateless components

--- parallel_async.js
+++ parallel_async.js
@@ -1,54 +1,46 @@
-ES7 brings async/await to, for the most part, replace promises for asynchronous function calls. But promises still have some fancy tricks up their sleeves that aren't going away just yet.<br />
-<br />
+ES7 brings async/await to, for the most part, replace promise chains for asynchronous function calls. But promises still have some fancy tricks up their sleeves that can't be abstracted out.<br />
 <br />
 <b>
 <span style="font-size: large;">If promises dont rely on each other, run them in parallel</span></b><br />
 Use Promise.all like a synchronous "checkpoint" and make sure all of the promises you ran asynchronously (without await) end up being resolved by the time your code actually needs them.<br />
 <br />
-<br />
 <span style="font-size: large;"><b>Sample Promises</b></span>
 <br />
-<pre data-codetype="auto"><code>
-const sendText = text =&gt; {
+<pre data-codetype="auto"><code>const sendText = text =&gt; {
   return new Promise(function(resolve, reject) {
     setTimeout( _ =&gt; resolve(text), 2000)
   });
 }
 
-const chainText = (prevText, newText) =&gt; {
+const chainText = (prevText, newText) =&gt; { //relies on value from sendText
   return new Promise(function(resolve, reject) {
     setTimeout( _ =&gt; resolve(prevText + newText), 2000)
   });
 }
 </code></pre>
 <br />
-<br />
 <span style="font-size: large;"><b>Must be synchronous</b></span>
 <br />
-<pre data-codetype="auto"><code>
-const text = await sendText('Sally')
+<pre data-codetype="auto"><code>const text = await sendText('Sally')
 const chainedText = await chainText(text, ' loves dogs')
 console.log(chainedText);
 //takes 4 seconds to finish
 </code></pre>
 <br />
-<b><br /></b>
 <span style="font-size: large;"><b>Needlessly chained</b></span>
 <br />
-<pre data-codetype="auto"><code>
-const text = await sendText('Sally')
-const otherText = await sendText(' loves dogs')
+<pre data-codetype="auto"><code>const text = await sendText('Sally')
+const otherText = await sendText(' loves dogs') //doesn't rely on the function above it
 console.log(text + otherText);
 //4 seconds to finish
 </code></pre>
 <br />
-<b><br /></b>
 <span style="font-size: large;"><b>Parallel, non-chained</b></span>
 <br />
-<pre data-codetype="auto"><code>
-const text = sendText('Sally')
+<pre data-codetype="auto"><code>const text = sendText('Sally')
 const otherText = sendText(' loves dogs')
 const [first, second] = await Promise.all([text, otherText])
+//checkpoint, using Promise.all, to ensure all promises are resolved before continuing
 console.log(first + second);
 //takes 2 seconds to finish
 </code></pre>
---
+++ stateless_components.js
@@ -0,0 +1,32 @@
+Stateless is good. It's a fundamental strength of functional programming. React components are class based, but basic components, that don't need a this.state, can be written as an arrow function.<br />
+<br />
+<span style="font-size: large;"><b>Why stateless&nbsp;</b></span><br />
+Stateless components are predictable because they do not have their own <b><code>this</code></b>&nbsp;variable that can be sneakily manipulated in a function. An arrow function's <b><code>this</code></b>&nbsp;is inherited from the scope above it. If you are making a component that doesn't use any React.js features (aside from jsx) or <b><code>this</code></b>, you can probably turn it into an arrow function component. Less overhead, less lines and more predictable behavior.<br />
+<br />
+<b><span style="font-size: large;">Stateful Component</span></b>
+<br />
+<pre data-codetype="auto"><code>import React, { Component } from 'react'
+class ProfilePicture extends Component {
+  render() {
+    return (
+      &ltdiv&gt
+        &ltimg src={this.props.imageUrl}/&gt
+        {this.props.name}
+        {this props.userDescription}
+      &lt/div&gt
+    )
+  }
+}
+
+</code></pre>
+<b><span style="font-size: large;">Stateless Component</span></b>
+<br />
+<pre data-codetype="auto"><code>import React from 'react'
+const ProfilePicture = props =&gt; (
+  &ltdiv&gt;
+    &ltimg src={props.imageUrl}/&gt
+    {props.name}
+    {props.userDescription}
+  &lt/div&gt
+) //parentheses for implicit return
+</code></pre>

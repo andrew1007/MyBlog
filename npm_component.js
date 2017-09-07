d33f2bebeec5738f369f62a7d97dd909160d0208
andrew1007 <yueh.andrew@gmail.com>
2017-09-07 16:18:12 -0700

    uploaded npm

--- npm_component.js
+++ npm_component.js
@@ -1,43 +1,65 @@
 <h2>
-  I solved my most difficult problem and I shared it with npm
+  I solved my most difficult problem and shared my solution with npm
 </h2>
 
 <p>
   I was having trouble making image rendering tidy and clean in
   a project I'm currently brushing up on. I solved it using
-  a component as a wrapper that controls CSS based on whether or
-  not all images were loaded inside the wrapper's children.
+  a component as a wrapper that controls render based on whether or
+  not all images were loaded inside the wrapper's children. I
+  couldn't find any solutions online so I published it to npm.
 </p>
 
 <p>
-  You can get my package react-on-images-loaded here. It's a more versatile
+  Its name is
+  <a target="_blank" href="https://www.npmjs.com/package/react-on-images-loaded">
+    <code>
+      react-on-images-loaded.
+    </code>
+  </a>
+  Check out my
+  <a target="_blank" href="https://andrew1007.github.io/react-on-images-loaded/">
+    demo
+  </a>
+  and see how you like it.
+  It's a more versatile
   version of my original solution. It's okay if you hate it and
-  uninstall and it's only natural to run into problems. If either of those
+  uninstall. You might run into problems as well. If either of those
   happen, kindly take the time to tell me what's
   happening or send me the hateful message
-  <a href={"https://github.com/andrew1007/react-on-images-loaded/issues"}>
+  <a target="_blank" href="https://github.com/andrew1007/react-on-images-loaded/issues">
     here
   </a>! :)
 </p>
 
-<h2>
+<b><h2 style="margin-bottom:-5px">
   It was shocking easy to make my own package
-</h2>
+</h2></b>
 
 <p>
-  In the same way create-react-app makes setting up a React.js app
-  a breeze,
-  react-component exists for anyone looking to publish their own
+  In the same way
+  <a target="_blank" href="https://github.com/facebookincubator/create-react-app">
+  <code>
+    create-react-app
+  </code>
+  </a>
+  makes setting up a React.js app a breeze,
+  <a target="_blank" href="https://github.com/JedWatson/generator-react-component">
+  <code>
+    generator-react-component
+  </code>
+  </a>
+  exists for anyone looking to publish their own
   npm package. Note that you have to make your own repo when using
   react-component. That's the only thing it won't do automatically.
 </p>
 
 <h2>
-  Key commands for react-component
+  Key commands for generator-react-component
 </h2>
 
-<p>
-  react-component gives you 2 all-encompassing terminal commands
+<p style="margin-bottom:-17px">
+  <code>generator-react-component</code> gives you 2 all-encompassing terminal commands
   to transpile, publish, and upload changes to your sample website.
   But I'm not a fan, because any error in one of those operations
   halts them all. Here are the ones I use.
@@ -45,17 +67,16 @@
 
 <pre data-codetype="auto">
   <code>npm run build //transpiles example website and component to ES5
-    npm run publish:site //uploads example.js website to your github repo
-    npm version <`newversionnum`> //sets new version of package before you
-    //upload to npm
-    npm publish //publishes package to npm
+npm run publish:site //uploads example.js website to your github repo
+npm version <`newversionnum`> //sets new version of package before you
+//upload to npm
+npm publish //publishes package to npm
   </code>
 </pre>
 
-
-<h2>
+<b><h2 style="margin-bottom:-5px">
   npm gives you 24 hours to unpublish any awful releases
-</h2>
+</h2></b>
 
 <p>
   When you have precisely zero downloads and are messing up left and right,
